1ee0a0180a2418c5a8b08a390bfeadfc4ad71962
Tatyana Gladka <tgladka@promptlink.com>
2020-11-22 22:51:17 +0200

    lab4

---
+++ MobileDevelopment/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MobileDevelopment
+//
+//  Created by Tatyana Gladka on 9/26/20.
+//  Copyright © 2020 Tatyana Gladka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
